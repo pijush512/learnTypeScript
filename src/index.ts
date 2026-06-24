a21f607704e74faaef445e7df00d8e1067cc60db
pijush512 <psagorsarker512@gmail.com>
2026-06-24 18:33:43 +0600

    Try Primitive data type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 let str: string = "Hello TypeScript";
 console.log(str);
+
+// !! Data Type
+const name: string = "Jac";
+const age: number = 23;
+const isActive: boolean = true;
+
+console.log(name, age, isActive);

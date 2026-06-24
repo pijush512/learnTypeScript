a040d0599280df4f7ba8c50e00541d714a569fe2
pijush512 <psagorsarker512@gmail.com>
2026-06-24 18:38:47 +0600

    Try create  Array

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,10 @@ const age: number = 23;
 const isActive: boolean = true;
 
 console.log(name, age, isActive);
+// Array
+let digits: number[] = [1, 5, 8, 9];
+let names: string[] = ["jak", "Mika", "Lili"];
+console.log(digits, names);
+// Object
+
+let student:
